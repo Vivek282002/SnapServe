ef8adbec81f8473b5a20bc85ee82f2443008508d
VIVEK <vivek212@gmail.com>
2025-08-08 16:13:28 +0530

    fix import paths for case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import './App.css'
 import Navbar from "./components/Navbar"
-import Home from './pages/home'
+import Home from './pages/Home'
 import Food from './pages/Food'
 import Grocery from './pages/Grocery'
 import Ride from './pages/Ride'
